30f594f05ceba0584f564ea1cc69ec1368fd8a9a
TomaszBud <56270317+TomaszBud@users.noreply.github.com>
2022-05-06 08:46:05 +0200

    negative errors without conflicts

--- STSP.py
+++ STSP.py
@@ -1,36 +1,56 @@
-from sekwencjonowanie import get_neighbour_matrix, append_new_oligonucletide
+from sekwencjonowanie import get_neighbour_matrix, append_new_oligonucleotide
 
-matrix = []
-with open("500+50.txt", "r", encoding="UTF-8") as file:
-    for line in file:
-        matrix.append(line.strip("\n"))
+if __name__ == "__main__":
+    matrix = []
+    with open("200-40-2", "r", encoding="UTF-8") as file:
+        for line in file:
+            matrix.append(line.strip("\n"))
 # print(matrix)
 
 
-length = 200
-cost = 0
-gain = 0
-neighbours = get_neighbour_matrix(matrix)
-o_le = len(matrix[1])
-# print(o_le)
-lowest = 1
-while any(matrix) < length:
-    for i in range(len(neighbours)):
-        for j in range(len(neighbours[i])):
-            if neighbours[i][j] == lowest and i != j:
-                print(len(matrix[i])-lowest, len(matrix[i]), lowest, matrix[j])
-                matrix[i] = matrix[i] + matrix[j][len(matrix[i])-lowest:]
-                matrix.pop(j)
-                for k in range(len(neighbours)):
-                    neighbours[k].pop(j)
-                neighbours.pop(j)
-                append_new_oligonucletide(matrix, matrix[i], neighbours, i, j)
+    length = 209
+    cost = 0
+    gain = 0
+    neighbours = get_neighbour_matrix(matrix)
+    o_le = len(matrix[1])
+    # print(o_le)
+    lowest = 1
+    i = 0
+    j = 0
+    leng = len(neighbours)
+    print(neighbours, matrix)
+    while (not any([len(x) >= length for x in matrix])) and len(matrix) != 1:
+        while i < leng:
+            while j < leng:
+                if neighbours[i][j] == lowest and i != j:
+                    # print(len(matrix[i])-lowest, len(matrix[i]), lowest, matrix[j])
+                    neighbours = append_new_oligonucleotide(matrix, neighbours, i, j, lowest, o_le)
+                    leng = len(neighbours)
+                    if i >= leng:
+                        i = leng - 1
+                    if j >= leng:
+                        j = leng - 1
+                    # neighbours = get_neighbour_matrix(matrix)
+                    # print(neighbours, "\n", matrix)
+                j += 1
+            i += 1
+            j = 0
+        if not any([y == lowest for y in neighbours.flatten()]):
+            lowest += 1
+            print(f"Lowest {lowest}")
 
-    if not any(neighbours) == lowest:
-        lowest += 1
+        i = 0
+        print(len(matrix))
 
 
-print(matrix)
-for i in range(len(neighbours)):
-    print(matrix[i], neighbours[i])
-    
+
+
+    # [x for x in range(10)]
+
+    # print(neighbours)
+
+
+    # print(matrix)
+    # for i in range(len(neighbours)):
+    #     print(matrix[i], neighbours[i])
+
--- sekwencjonowanie.py
+++ sekwencjonowanie.py
@@ -1,61 +1,64 @@
 import numpy as np
+
+
 def get_neighbour_matrix(matrix):
-    neighbours = []
-    for i in range(len(matrix)):
-        neighbours.append([])
-        for j in range(len(matrix)):
+    l = len(matrix)
+    neighbours = np.empty([l, l])
+    for i in range(l):
+        for j in range(l):
             flag = False
-            for k in range(len(matrix[i])):
-                if matrix[i][k:len(matrix[i])] == matrix[j][0:len(matrix[i]) - k]:
-                    neighbours[i].append(k)
+            for k in range(len(matrix[j])):
+                if matrix[i][k:] == matrix[j][:len(matrix[i]) - k]:
+                    neighbours[i][j] = k
                     flag = True
                     break
             if not flag:
-                neighbours[i].append(10)
-
+                neighbours[i][j] = len(matrix[0])
     return neighbours
 
 
-def append_new_oligonucletide(matrix, oligonucleotide, neighbours, old1, old2):
-    for j in range(len(matrix)):
-        flag = False
-        for k in range(len(matrix[old1])):
-            if matrix[old1][k:len(matrix[old1])] == matrix[j][0:len(matrix[old1]) - k]:
-                neighbours[old1][old2] = k
-                neighbours[old2][old1] = k
-                flag = True
-                break
-        if not flag:
-            neighbours[old1].append(10)
+def append_new_oligonucleotide(names, neighbours, old_row: int, old_col: int, offset: int, o_le: int):
+    neighbours[:, old_col] = neighbours[:, old_row]
+    neighbours[old_col][old_col] = 0
+    neighbours = np.delete(neighbours, old_row, 0)
+    neighbours = np.delete(neighbours, old_row, 1)
+
+    # names[old_col] = names[old_row][:offset + len(names[old_row]) - o_le] + names[old_col]
+
+    print(names[old_row] + '\n' + " "*(len(names[old_row]) - o_le + offset) + names[old_col])
+
+    # print(names[old_row][:offset], names[old_col][:])
+    names[old_col] = names[old_row][:len(names[old_row]) - o_le + offset] + names[old_col][:]
+    print(names[old_col] + '\n')
+
+    names.pop(old_row)
+    return neighbours
 
 
-
-
-
-matrix = []
-with open("500+50.txt", "r", encoding="UTF-8") as file:
-    for line in file:
-        matrix.append(line.strip("\n"))
-
-print(matrix)
-
-neighbours = get_neighbour_matrix(matrix)
-for i in range(len(neighbours)):
-    print(matrix[i], neighbours[i])
-print(len(neighbours[0]) == len(neighbours[1]))
-
-
-ones = []
-ones_c = 0
-sumki = 0
-for i in range(len(neighbours)):
-    ones.append([])
-    for j in range(len(neighbours[i])):
-        if neighbours[i][j] == 2:
-            ones_c += 1
-            ones[i].append(1)
-        else:
-            ones[i].append(0)
-    # print(sum(ones[i]))
-    print(np.mean(neighbours[i]))
-print(ones_c, sumki)
+# matrix = []
+# with open("krotki.txt", "r", encoding="UTF-8") as file:
+#     for line in file:
+#         matrix.append(line.strip("\n"))
+#
+# print(matrix)
+#
+# neighbours = get_neighbour_matrix(matrix)
+# for i in range(len(neighbours)):
+#     print(matrix[i], neighbours[i])
+# print(len(neighbours[0]) == len(neighbours[1]))
+#
+#
+# ones = []
+# ones_c = 0
+# sumki = 0
+# # for i in range(len(neighbours)):
+# #     ones.append([])
+# #     for j in range(len(neighbours[i])):
+# #         if neighbours[i][j] == 1:
+# #             ones_c += 1
+# #             ones[i].append(1)
+# #         else:
+# #             ones[i].append(0)
+# #     # print(sum(ones[i]))
+# #     print(np.mean(neighbours[i]))
+# print(ones_c, sumki)
